447ad271ad38b07a7c4391ad01f4409eb0426685
Antonin <carpentier.antonin.1@gmail.com>
2025-07-09 11:34:04 +0200

    failure args added

--- src/main.py
+++ src/main.py
@@ -16,6 +16,7 @@ def main():
     
     # if it is the capacitated version, add the argument
     parser.add_argument('--capacitated', action='store_true', help='If set, the solver will handle capacitated p-center problem instances.')
+    parser.add_argument('--failure', action='store_true', help='If set, the solver will handle p-center problem instances with failure foresight.')
     args = parser.parse_args()
     
     if args.file:
@@ -26,11 +27,18 @@ def main():
 
     # Instance reading
     is_capacitated = False
+    is_failure = False
     model_class = "classical"
-    if args.capacitated:
+    if args.failure:
+        # If the instance is with failure foresight, read the failure foresight instance
+        is_failure = True
+        model_class = "failure"
+    elif args.capacitated:
         # If the instance is capacitated, read the capacitated instance
         is_capacitated = True
         model_class = "capacitated"
+    
+        
     instance_data = read_instance(file_path, capacitated=is_capacitated)
 
     # Problem solving
